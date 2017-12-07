00f27108099062d0254cb61588ba25265b847f98
William Strong <william.strong@me.com>
2017-12-07 12:03:28 -0800

    work to update db with info

--- social_networks/data/congress.py
+++ social_networks/data/congress.py
@@ -1,8 +1,10 @@
 from social_networks.data import __congress__
+from social_networks.database_controller import *
+import pickle
+import json
 
 
 def congress_users():
-    import pickle
 
     with open(__congress__, 'rb') as f:
         congress_users = pickle.load(f)
@@ -10,13 +12,120 @@ def congress_users():
     tmp = []
 
     for user in congress_users:
-        tmp.append(user.screen_name)
+        tmp.append({'twitter_name':user.screen_name, 'name':user.name})
 
     return tmp
 
+
+def add_name():
+    # Current congress data
+    r = ReadFromDatabase('data', 'congress_map_trimmed')
+    data = r.read_raw_data()
+
+    c = congress_users()
+
+
+
+    count = 0
+
+    tmp_stuff = []
+    for i in data:
+        for j in c:
+            if i['user'] == j['twitter_name']:
+                i['name'] = j['name']
+                count += 1
+                tmp_stuff.append(i)
+
+    Database('data').remove_collection('congress_map_trimmed_with_name')
+    w = WriteToDatabase('data', 'congress_map_trimmed_with_name')
+    w.collection.insert_many(tmp_stuff)
+    # print(data[0])
+
+def add_party():
+    r = ReadFromDatabase('data', 'congress_map_trimmed_with_name')
+    data = r.read_raw_data()
+
+    with open('congress_members.json', 'r') as f:
+        info = f.read()
+
+    info = json.loads(info)
+
+
+    results = [res for res in data]
+    data.close()
+    for i in results:
+        i['name'] = i['name'].replace("Sen. ", '')
+        i['name'] = i['name'].replace('Dr. ', '')
+        i['name'] = i['name'].replace('Rep. ', '')
+        i['name'] = i['name'].replace('Rep.', '')
+        i['name'] = i['name'].replace('Rep ', '')
+        i['name'] = i['name'].replace('U.S. Rep ', '')
+        i['name'] = i['name'].replace('U.S. ', '')
+        i['name'] = i['name'].replace('US Rep ', '')
+        i['name'] = i['name'].replace('Senator ', '')
+        i['name'] = i['name'].replace(', MD', '')
+        i['name'] = i['name'].replace('Dr. ', '')
+        i['name'] = i['name'].replace('Governor ', '')
+        i['name'] = i['name'].replace('Congressman ', '')
+
+        if i['name'] == 'CathyMcMorrisRodgers':
+            i['name'] = 'Cathy McMorris Rodgers'
+
+        if i['name'] == "Glenn 'GT' Thompson":
+            i['name'] = 'Glenn Thompson'
+
+        if i['name'] == "johnculberson":
+            i['name'] = 'John Culberson'
+
+        r.collection.save(i)
+
+    matched = []
+    count = 0
+    new_col = []
+    for i in info:
+        # print(i)
+        # data.rewind()
+        for j in results:
+            # a = i['name']
+            # b = j['name']
+            # print(a)
+            # print(b)
+            if i['name'] == j['name']:
+                # Add to matched
+                matched.append(i['name'])
+                count += 1
+
+                # Add new data
+                j['party'] = i['party']
+                j['district'] = i['district']
+                j['state'] = i['state']
+                j['years'] = i['years']
+                r.collection.save(j)
+                new_col.append(j)
+
+
+
+    Database('data').remove_collection('current_congress_with_data')
+    WriteToDatabase('data', 'current_congress_with_data').collection.insert_many(new_col)
+
+    data.close()
+
+
 if __name__ == "__main__":
-    for i in congress_users():
+    # congress_users()
+    # congress_data()
+
+    add_name()
+    add_party()
+
+    a = ReadFromDatabase('data', 'current_congress_with_data').read_raw_data()
+    for i in a:
         print(i)
+    # data = r.read_raw_data()
+    # for i in data:
+    #     print(i)
+
+
 
 """
 JoeLieberman
