8c544dcea4a09428a53c8a86be210132f37ac9b3
pattpattpatt <alex.j.patton@biola.edu>
2017-12-07 23:08:36 -0800

    updated make_graph to reflect new attributes

--- social_networks/analysis/alex_graphing.py
+++ social_networks/analysis/alex_graphing.py
@@ -9,12 +9,26 @@ g = maker.g
 pos = arf_layout(g)
 
 tree_map = min_spanning_tree(g)
-print(tree_map)
+#print(tree_map)
 vert_list = find_edge(g, tree_map, 1)
-print(vert_list)
+#print(vert_list)
 
-vprops = {'anchor': 0, 'pen_width': 0.2}
+
+print("In degree: {0} | Out degree: {1} | Total degree: {2}".format(vertex_average(g, deg="in")[0], vertex_average(g, deg="out")[0], vertex_average(g, deg="total")[0]))
+
+vprops = {'anchor': 0, 'pen_width': 0.2, 'font_size': 8}
 eprops = {'pen_width': 0.4}
-graph_draw(g, pos=pos, vertex_font_size=8,
-           vprops=vprops, eprops=eprops,
-           output_size=(1000, 1000), output="graphs/graph.png")
+color = g.vp.color
+state = minimize_nested_blockmodel_dl(g)
+draw_hierarchy(state,
+               bg_color=[0, 0, 0, 1],
+               vertex_fill_color=color,
+               vertex_color=color,
+               output="graphs/nested_model.png",
+               size=5)
+
+# graph_draw(g, pos=pos,
+#             vprops=vprops, eprops=eprops,
+#             output_size=(1000, 1000),
+#             bg_color=[1.0, 2.0, 3.0, 4.0],
+#             output="graphs/test.png")
--- social_networks/analysis/make_graph.py
+++ social_networks/analysis/make_graph.py
@@ -3,13 +3,31 @@ from social_networks.database_controller import *
 
 
 class MakeGraph:
-    def __init__(self, database="data", collection_name="congress_map_trimmed"):
+    def __init__(self, database="data", collection_name="current_congress_with_data"):
         self.reader = ReadFromDatabase(database, collection_name)
         self.cursor = self.reader.read_raw_data()
         self.g = Graph()
         self.g.add_vertex(self.cursor.count())
-        self.add_vertex_property('string', 'user_name')
-        self.add_vertex_prop_val()
+        self.add_vertex_property('string', 'name')
+        self.add_vertex_prop_val('name')
+        self.cursor.rewind()
+        self.add_vertex_property('string', 'user')
+        self.add_vertex_prop_val('user')
+        self.cursor.rewind()
+        self.add_vertex_property('string', 'party')
+        self.add_vertex_prop_val('party')
+        self.cursor.rewind()
+        self.add_vertex_property('vector<double>', 'color')
+        self.add_vertex_prop_val('color')
+        self.cursor.rewind()
+        self.add_vertex_property('string', 'district')
+        self.add_vertex_prop_val('district')
+        self.cursor.rewind()
+        self.add_vertex_property('string', 'state')
+        self.add_vertex_prop_val('state')
+        self.cursor.rewind()
+        self.add_vertex_property('string', 'years')
+        self.add_vertex_prop_val('years')
         self.cursor.rewind()
         self.add_graph_edges()
 
@@ -18,25 +36,31 @@ class MakeGraph:
         prop = self.g.new_vertex_property(prop_type)
         self.g.vertex_properties[prop_name] = prop
 
-    def add_vertex_prop_val(self):
+    def add_vertex_prop_val(self, prop_value):
         # add names to each vertex
         for v in self.g.vertices():
-            self.g.vp.user_name[v] = self.cursor[self.g.vertex_index[v]]['user']
-            # print(self.g.vertex_properties['user_name'][v])
+            if prop_value == 'color':
+                if self.cursor[self.g.vertex_index[v]]['party'] == 'Republican':
+                    self.g.vertex_properties[prop_value][v] = (1, 0, 0, 1)
+                else:
+                    self.g.vertex_properties[prop_value][v] = (0, 0, 1, 1)
+            else:
+                self.g.vertex_properties[prop_value][v] = self.cursor[self.g.vertex_index[v]][prop_value]
+            # print(self.g.vertex_properties['name'][v])
 
     def add_graph_edges(self):
         # create all edges
         for user in self.cursor:
-            # v1 is the vertex where name = cursor['user']
-            v1 = find_vertex(self.g, self.g.vp.user_name, user['user'])[0]
+            # v1 is the vertex where name = cursor['name']
+            v1 = find_vertex(self.g, self.g.vp.user, user['user'])[0]
             for mention in user['user_mentions']:
                 try:
                     # v2 is the vertex where name = mention
-                    v2 = find_vertex(self.g, self.g.vp.user_name, mention)[0]
+                    v2 = find_vertex(self.g, self.g.vp.user, mention)[0]
                 except IndexError:
-                    print("Error: " + mention + " is not in the collection")
+                    #print("Error: " + mention + " is not in the collection")
                     continue
 
-                if self.g.vp.user_name[v1] != self.g.vp.user_name[v2]:
-                    # print("adding edge between " + self.g.vp.user_name[v1] + " and " + self.g.vp.user_name[v2])
+                if self.g.vp.user[v1] != self.g.vp.user[v2]:
+                    #print("adding edge between " + self.g.vp.user[v1] + " and " + self.g.vp.user[v2])
                     self.g.add_edge(v1, v2)
